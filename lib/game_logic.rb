a92c4d63fc765fe9e3882a81ce74b1e998b0a441
BZAgustin <abustoszanelli@gmail.com>
2022-07-30 15:21:26 -0300

    - Update #valid_input? to match commands - Add #valid_save? - Add comments

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,18 +2,22 @@
 
 # game logic
 module GameLogic
-  def letter_present?(letter, word)
-    word.include?(letter)
+  # determines if input on player_turns is an alphabetical letter or a command
+  def valid_input?(input)
+    (input.length == 1 && input.match(/[a-zA-Z]/)) || %w[save quit].include?(input.downcase)
   end
 
-  def valid?(input)
-    input.length == 1 && input.match(/[a-zA-Z]/) && input != ''
+  # determines if input is a single-digit number
+  def valid_save?(input)
+    input.length == 1 && input.match(/\d/)
   end
 
+  # self-explanatory
   def word_complete?(guess, word)
     guess == word
   end
 
+  # fill the correspondent placeholder(s) with the letter provided
   def update_guess(letter, word, guess)
     word.each_index { |index| guess[index] = letter if word[index] == letter }
     guess
