eb8eed4eddac026ebb0b57442d660f21846f4420
BZAgustin <abustoszanelli@gmail.com>
2022-07-30 15:15:59 -0300

    - Move #select_random_word to TxtParser module - Add comments

--- lib/txt_parser.rb
+++ lib/txt_parser.rb
@@ -2,9 +2,14 @@
 
 # read and filter the text file
 module TxtParser
+  # open words.txt and remove every word less than 6 characters and more than 12 characters long
   def read_filtered_list
     words = File.open('./words.txt')
 
     words.reject { |line| line.length < 6 || line.length > 13 }
+  end
+
+  def select_random_word
+    read_filtered_list.sample
   end
 end
